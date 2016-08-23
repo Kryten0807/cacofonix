b68eea7502f899e2bf0fb7cd1dccd34af3570e69
Dave Thomas <dave@monstergfx.com>
2016-08-23 08:38:34 -0700

    code cleanup

--- src/components/Label.js
+++ src/components/Label.js
@@ -2,15 +2,27 @@
 //
 import React from 'react';
 
+/**
+ * The Label component
+ * @param  {Boolean} required A flag indicating whether this label should be
+ *                            postfixed with the required asterisk
+ * @param  {String}  label    The text to display in the label component
+ * @return {React.Element}    The React Element describing this component
+ */
 const Label = ({ required, label }) => (
     <label className="control-label">
+
         {label}
+
         {required
             && <sup style={{ color: 'red' }}>
                 <i className="glyphicon glyphicon-asterisk" />
             </sup>
         }
+
     </label>
 );
 
+// export the component
+//
 export default Label;
