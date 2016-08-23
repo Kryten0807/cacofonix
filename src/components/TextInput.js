6d8cd86b452e25e152694752559f9d6c0de1851b
Dave Thomas <dave@monstergfx.com>
2016-08-23 12:50:52 -0700

    code cleanup

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -66,11 +66,11 @@ class TextInput extends React.Component {
 // set the property types for the TextInput component
 //
 TextInput.propTypes = {
-    required:    React.PropTypes.bool,
-    label:       React.PropTypes.string,
-    description: React.PropTypes.string,
-    placeholder: React.PropTypes.string,
-    value:       React.PropTypes.oneOfType([
+    required:     React.PropTypes.bool,
+    label:        React.PropTypes.string,
+    description:  React.PropTypes.string,
+    placeholder:  React.PropTypes.string,
+    value:        React.PropTypes.oneOfType([
         React.PropTypes.string,
         React.PropTypes.number,
     ]),
