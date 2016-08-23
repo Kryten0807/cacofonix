34823b3558896abdde65b85b8b91e820624942b3
Dave Thomas <dave@monstergfx.com>
2016-08-23 10:11:24 -0700

    comment cleanup

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,7 +3,19 @@
 import React from 'react';
 import classnames from 'classnames';
 
+/**
+ * The Alert component
+ * @param  {String}              style       The style of the alert
+ * @param  {Boolean}             dismissible If true, then the alert is
+ *                                           dismissible & will include a
+ *                                           button which will remove the
+ *                                           component when clicked
+ * @param  {Array|React.Element} children    The child (or children) to display in this component
+ * @return {React.Element}                   The React Element representing this component
+ */
 const Alert = ({ style, dismissible, children }) => {
+    // determine the classes for the outermost div
+    //
     const classes = classnames('alert', {
         'alert-danger':      style === 'danger' || style === 'error',
         'alert-warning':     style === 'warning' || style === 'warn',
@@ -11,6 +23,9 @@ const Alert = ({ style, dismissible, children }) => {
         'alert-success':     style === 'success' || style === 'ok' || !style,
         'alert-dismissible': dismissible,
     });
+
+    // return the React Element
+    //
     return (
         <div className={classes}>
             {dismissible &&
@@ -37,4 +52,6 @@ Alert.propTypes = {
     ]),
 };
 
+// export the component
+//
 export default Alert;
