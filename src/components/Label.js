ac66db514dfeb5e64196ba39787ce9ed88be907c
Dave Thomas <dave@monstergfx.com>
2016-08-23 09:18:47 -0700

    implemented htmlFor property

--- src/components/Label.js
+++ src/components/Label.js
@@ -9,8 +9,8 @@ import React from 'react';
  * @param  {String}  label    The text to display in the label component
  * @return {React.Element}    The React Element describing this component
  */
-    <label className="control-label">
 const Label = ({ htmlFor, required, label }) => (
+    <label htmlFor={htmlFor} className="control-label">
 
         {label}
 
