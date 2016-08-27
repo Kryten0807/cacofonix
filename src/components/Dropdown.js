a73df65134452dc7358b575a2349ec08d679eda4
Dave Thomas <dave@monstergfx.com>
2016-08-26 19:26:44 -0700

    added label to component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,12 @@
 // dependencies
 //
 import React from 'react';
+import Label from './Label';
 
-const Dropdown = () => (<div className="form-group" />);
+const Dropdown = (props) => (
+    <div className="form-group">
+        {props.label ? <Label label={props.label} /> : ''}
+    </div>
+);
 
 export default Dropdown;
