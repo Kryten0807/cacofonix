e2804ee7d0b58cd6fc46c11de7c3c10116c3c2fb
Dave Thomas <dave@monstergfx.com>
2016-08-29 13:53:33 -0700

    code cleanup

--- src/components/NumericInput.js
+++ src/components/NumericInput.js
@@ -15,7 +15,14 @@ class NumericInput extends React.Component {
     render() {
         return (
             <div className="form-group">
-                {this.props.label ? <Label htmlFor={this.id} required={!!this.props.required} label={this.props.label} /> : ''}
+                {this.props.label
+                    ? <Label
+                        htmlFor={this.id}
+                        required={!!this.props.required}
+                        label={this.props.label}
+                    />
+                    : ''
+                }
                 <input
                     type="text"
                     id={this.id}
