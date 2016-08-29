417830c80a3b3e1436a02f475a5dffdf241d3e3a
Dave Thomas <dave@monstergfx.com>
2016-08-29 14:27:11 -0700

    added onBlur method

--- src/components/NumericInput.js
+++ src/components/NumericInput.js
@@ -17,6 +17,16 @@ class NumericInput extends React.Component {
     }
 
     onBlur(event) {
+        const newState = this.validate(event.target.value);
+        newState.hasValidated = true;
+
+        this.setState(newState);
+
+        if (this.props.onValidation) {
+            this.props.onValidation(newState.hasValidated, newState.isValid, newState.validationMessage);
+        }
+    }
+
     }
 
     render() {
