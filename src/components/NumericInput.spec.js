ae1c861b53ae888b4971aa85486ea542ad88f422
Dave Thomas <dave@monstergfx.com>
2016-08-29 13:29:12 -0700

    added initial numericinput spec file

---
+++ src/components/NumericInput.spec.js
@@ -0,0 +1,12 @@
+// the eslint "no-unused-expressions" rule is triggered by the `expect`
+// assertions in the tests. So, in order to avoid problems with eslint, I'm
+// disabling that rule in this file
+//
+/* eslint-disable no-unused-expressions */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import chai from 'chai';
+import NumericInput from './NumericInput';
+
+const expect = chai.expect;
