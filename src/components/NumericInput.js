010366299e0b8e1d8ac07bba80d91cd95c5d40ae
Dave Thomas <dave@monstergfx.com>
2016-08-29 13:43:03 -0700

    added label to component

--- src/components/NumericInput.js
+++ src/components/NumericInput.js
@@ -3,8 +3,11 @@
 import React from 'react';
 
 const NumericInput = () => (
+import Label from './Label';
+
     <div className="form-group">
         <input type="text" className="form-control" />
+        {props.label ? <Label label={props.label} /> : ''}
     </div>
 );
 
