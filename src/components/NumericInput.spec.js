d4a6eff9d8afe4019d00a71c73294112bf0c4fd8
Dave Thomas <dave@monstergfx.com>
2016-08-29 13:30:30 -0700

    added initial specs for numeric input

--- src/components/NumericInput.spec.js
+++ src/components/NumericInput.spec.js
@@ -10,3 +10,25 @@ import chai from 'chai';
 import NumericInput from './NumericInput';
 
 const expect = chai.expect;
+
+/* *****************************************************************************
+in terms of basic markup, the NumericInput component
+    should be a div.form-group
+    should include an input[type="text"]
+    should include an input.form-control
+    should include a label with the appropriate text if a label is specified
+    should include a placeholder if a placeholder is specified
+*/
+describe('in terms of basic markup, the NumericInput component', () => {
+
+    // it('should be a div.form-group', () => {});
+
+    // it('should include an input[type="text"]', () => {});
+
+    // it('should include an input.form-control', () => {});
+
+    // it('should include a label with the appropriate text if a label is specified', () => {});
+
+    // it('should include a placeholder if a placeholder is specified', () => {});
+
+});
