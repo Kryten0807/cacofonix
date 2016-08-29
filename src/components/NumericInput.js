dbc8ce0b55ab424b1baf69ce84b30a5e918d5cc4
Dave Thomas <dave@monstergfx.com>
2016-08-29 13:35:39 -0700

    added class to input element

--- src/components/NumericInput.js
+++ src/components/NumericInput.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 const NumericInput = () => (
     <div className="form-group">
-        <input type="text" />
+        <input type="text" className="form-control" />
     </div>
 );
 
