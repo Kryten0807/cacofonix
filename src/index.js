192728da540d6945fb5a5ad2879b3d9038d73198
Dave Thomas <dave@monstergfx.com>
2016-08-30 09:26:37 -0700

    added index file to amalgamate all exports

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// export the individual components
+//
+export * from 'components/Alert';
+export * from 'components/Dropdown';
+export * from 'components/Label';
+export * from 'components/NumericInput';
+export * from 'components/Panel';
+export * from 'components/TextInput';
