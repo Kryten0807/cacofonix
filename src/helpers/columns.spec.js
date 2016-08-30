51f29dc76d76ce433f07de135a7dfc4e9a24e47c
Dave Thomas <dave@monstergfx.com>
2016-08-30 12:18:57 -0700

    added specs for valid values

--- src/helpers/columns.spec.js
+++ src/helpers/columns.spec.js
@@ -26,7 +26,58 @@ the columns function
 */
 describe('the columns function', () => {
 
+    it('should return empty string when called with a null', () => {
+        expect(columns(null)).to.equal('');
+    });
 
-    // it('should return the correct string for a collection of keys', () => {});
+    it('should return empty string when called with an empty string', () => {
+        expect(columns('')).to.equal('');
+    });
+
+    it('should return empty string when called with an empty array', () => {
+        expect(columns([])).to.equal('');
+    });
+
+    it('should return empty string when called with a non-empty array', () => {
+        expect(columns([1, 2, 3])).to.equal('');
+    });
+
+    it('should return empty string when called with an empty object', () => {
+        expect(columns({})).to.equal('');
+    });
+
+    it('should return empty string when called with an object containing invalid keys', () => {
+        expect(columns({
+            this:  1,
+            is:    2,
+            not:   3,
+            valid: 4,
+        })).to.equal('');
+    });
+
+    it('should return the correct string for key = xs', () => {
+        expect(columns({ xs: 1 })).to.equal('col-xs-1');
+    });
+
+    it('should return the correct string for key = sm', () => {
+        expect(columns({ sm: 2 })).to.equal('col-sm-2');
+    });
+
+    it('should return the correct string for key = md', () => {
+        expect(columns({ md: 3 })).to.equal('col-md-3');
+    });
+
+    it('should return the correct string for key = lg', () => {
+        expect(columns({ lg: 4 })).to.equal('col-lg-4');
+    });
+
+    it('should return the correct string for a collection of keys', () => {
+        expect(columns({
+            xs: 12,
+            md: 8,
+            sm: 10,
+            lg: 6,
+        })).to.equal('col-xs-12 col-sm-10 col-md-8 col-lg-6');
+    });
 
 });
