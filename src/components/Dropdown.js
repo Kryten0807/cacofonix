402b43e8891e68bbc28753fae2a4a7c95d87b688
Dave Thomas <dave@monstergfx.com>
2016-08-30 12:51:03 -0700

    updated code to skip updated when value has not changed

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -74,26 +74,31 @@ class Dropdown extends React.Component {
         //
         newState.hasValidated = this.state.hasValidated;
 
-        // set the new state
+        // has the value changed? if not, then don't bother updating or calling
+        // event handlers
         //
-        this.setState(newState);
+        if (newState.value !== this.state.value) {
+            // set the new state
+            //
+            this.setState(newState);
 
-        // do we have an onValidation handler? has the value changed? if so,
-        // call the handler with the new validation state
-        //
-        if (this.props.onValidation && currentValue !== newState.value) {
-            this.props.onValidation(
-                newState.hasValidated,
-                newState.isValid,
-                newState.validationMessage
-            );
-        }
+            // do we have an onValidation handler? has the value changed? if so,
+            // call the handler with the new validation state
+            //
+            if (this.props.onValidation) {
+                this.props.onValidation(
+                    newState.hasValidated,
+                    newState.isValid,
+                    newState.validationMessage
+                );
+            }
 
-        // do we have an onChange handler? has the value changed? if so, call
-        // the handler with the new value
-        //
-        if (this.props.onChange && currentValue !== newState.value) {
-            this.props.onChange(newState.value);
+            // do we have an onChange handler? has the value changed? if so, call
+            // the handler with the new value
+            //
+            if (this.props.onChange) {
+                this.props.onChange(newState.value);
+            }
         }
     }
 
