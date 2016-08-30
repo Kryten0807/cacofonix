1048d4145d3cc46d55c7c86909710c1e90c24249
Dave Thomas <dave@monstergfx.com>
2016-08-30 16:45:23 -0700

    added basic component

---
+++ src/components/DateInput.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import uniqueId from 'lodash/uniqueId';
+
+import Label from './Label';
+import columns from '../helpers/columns';
+
+class DateInput extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.id = uniqueId('DateInput-');
+    }
+    render() {
+        return (
+            <div className="form-group">
+                {this.props.label
+                    ? <Label
+                        htmlFor={this.id}
+                        required={this.props.required}
+                        label={this.props.label}
+                        className={columns(this.props.labelColumns)}
+                    />
+                    : ''
+                }
+                <div className={columns(this.props.inputColumns)}>
+                    <input id={this.id} type="text" readOnly={this.props.readOnly} className="form-control" placeholder={this.props.placeholder} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DateInput;
