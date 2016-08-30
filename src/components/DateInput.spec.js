4e54b4419abbe5557a34ca8c1d9c0737fdf7e47f
Dave Thomas <dave@monstergfx.com>
2016-08-30 16:45:13 -0700

    added markup specs

--- src/components/DateInput.spec.js
+++ src/components/DateInput.spec.js
@@ -31,28 +31,104 @@ the basic markup of the DateInput component
 */
 describe('the basic markup of the DateInput component', () => {
 
-    // it('should be a div.form-group', () => {});
-    
-    // it('should include an input[type="text"]', () => {});
-    
-    // it('should include an input.form-control', () => {});
-    
-    // it('should include a label with the appropriate text if a label is specified', () => {});
-    
-    // it('should include a placeholder if a placeholder is specified', () => {});
-    
-    // it('should include a label with the the required flag if a label is specified & required is set', () => {});
-    
-    // it('should not include a label with the the required flag if a label is specified & required is not set', () => {});
-    
-    // it('should not include a label with the the required flag if a label is not specified & required is set', () => {});
-    
-    // it('should have the readonly property on the input when readonly=true', () => {});
-    
-    // it('should not have the readonly property on the input when readonly=false', () => {});
-    
-    // it('should have the correct label widths when labelColumns is set', () => {});
-    
-    // it('should have the correct select widths when inputColumns is set', () => {});
-    
+    it('should be a div.form-group', () => {
+        const component = shallow(<DateInput />);
+
+        expect(component.is('div.form-group')).to.equal(true);
+    });
+
+    it('should include an input[type="text"]', () => {
+        const component = shallow(<DateInput />);
+
+        expect(component.find('input[type="text"]').length).to.equal(1);
+    });
+
+    it('should include an input.form-control', () => {
+        const component = shallow(<DateInput />);
+
+        expect(component.find('input.form-control').length).to.equal(1);
+    });
+
+    it('should include a label with the appropriate text if a label is specified', () => {
+        const label = 'this is silly';
+
+        const component = shallow(<DateInput label={label} />);
+
+        expect(component.find('Label').length).to.equal(1);
+        expect(component.find('Label').props().label).to.equal(label);
+    });
+
+    it('should include a placeholder if a placeholder is specified', () => {
+        const placeholder = 'this is silly';
+
+        const component = shallow(<DateInput placeholder={placeholder} />);
+
+        expect(component.find('input').props().placeholder).to.equal(placeholder);
+    });
+
+    it('should include a label with the the required flag if a label is specified & required is set', () => {
+        const required = true;
+        const label = 'this is silly';
+
+        const component = shallow(<DateInput required={required} label={label} />);
+
+        expect(component.find('Label').length).to.equal(1);
+        expect(component.find('Label').props().required).to.equal(required);
+
+    });
+
+    it('should not include a label with the the required flag if a label is specified & required is not set', () => {
+        const required = false;
+        const label = 'this is silly';
+
+        const component = shallow(<DateInput required={required} label={label} />);
+
+        expect(component.find('Label').length).to.equal(1);
+        expect(component.find('Label').props().required).to.equal(required);
+    });
+
+    it('should not include a label with the the required flag if a label is not specified & required is set', () => {
+        const required = true;
+
+        const component = shallow(<DateInput required={required} />);
+
+        expect(component.find('Label').length).to.equal(0);
+    });
+
+    it('should have the readOnly property on the input when readOnly=true', () => {
+        const readOnly = true;
+
+        const component = shallow(<DateInput readOnly={readOnly} />);
+
+        expect(component.find('input').props().readOnly).to.equal(readOnly);
+    });
+
+    it('should not have the readOnly property on the input when readOnly=false', () => {
+        const readOnly = false;
+
+        const component = shallow(<DateInput readOnly={readOnly} />);
+
+        expect(component.find('input').props().readOnly).to.equal(readOnly);
+    });
+
+    it('should have the correct label widths when labelColumns is set', () => {
+        const label = 'my label';
+        const columns = { xs: 10, md: 8 };
+        const expectedClass = 'col-xs-10 col-md-8';
+
+        const component = shallow(<DateInput label={label} labelColumns={columns} />);
+
+        expect(component.find('Label').props().className).to.equal(expectedClass);
+    });
+
+    it('should have the correct select widths when inputColumns is set', () => {
+        const label = 'my label';
+        const columns = { xs: 10, md: 8 };
+        const expectedClass = '.col-xs-10.col-md-8';
+
+        const component = shallow(<DateInput label={label} inputColumns={columns} />);
+
+        expect(component.find(`div${expectedClass} input`).length).to.equal(1);
+    });
+
 });
