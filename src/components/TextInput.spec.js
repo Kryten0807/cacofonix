564d01d1154a23017f938fe488b6dff2e9ed054d
Dave Thomas <dave@monstergfx.com>
2016-09-07 09:32:27 -0700

    updated ontes

--- src/components/TextInput.spec.js
+++ src/components/TextInput.spec.js
@@ -13,6 +13,12 @@ import TextInput from './TextInput';
 const expect = chai.expect;
 
 /* *****************************************************************************
+when the readOnly flag is set/not set, the TextInput component
+    should have an input with prop readOnly=true when readOnly=true
+    should have an input with prop readOnly=false when readOnly=false
+*/
+
+/* *****************************************************************************
 when the password flag is set/not set, the TextInput component
     should have an input[type="password"] when password=true
     should have an input[type="text"] when password=false
