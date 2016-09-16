7d324f59495a590dd6042522631d05f8d78b89d1
Dave Thomas <dave@monstergfx.com>
2016-09-16 13:25:38 -0700

    added inline, horizontal to proptypes

--- src/components/Form.js
+++ src/components/Form.js
@@ -79,7 +79,9 @@ class Form extends React.Component {
 // set the property types for the component
 //
 Form.propTypes = {
-    children: React.PropTypes.oneOfType([
+    inline:     React.PropTypes.bool,
+    horizontal: React.PropTypes.bool,
+    children:   React.PropTypes.oneOfType([
         React.PropTypes.arrayOf(React.PropTypes.node),
         React.PropTypes.node,
     ]).isRequired,
