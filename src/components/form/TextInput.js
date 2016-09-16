66ad1e62594e79acd131ca4220c615ffc09b717e
Dave Thomas <dave@monstergfx.com>
2016-09-16 11:53:57 -0700

    added TextInput component

--- src/components/form/TextInput.js
+++ src/components/form/TextInput.js
@@ -5,6 +5,18 @@ import classnames from 'classnames';
 
 
 class TextInput extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value:   props.value,
+            isValid: true,
+        };
+
+        this.onBlur = this.onBlur.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
     render() {
         return (<input type="text" />);
     }
