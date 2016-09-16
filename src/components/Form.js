eab0d2cbfd71c2c6e33e15f136ef887937dcdbaa
Dave Thomas <dave@monstergfx.com>
2016-09-16 10:04:10 -0700

    added TextInput component

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,10 +3,16 @@
 import React from 'react';
 import classnames from 'classnames';
 
+import TextInput from './form/TextInput';
+
 class Form extends React.Component {
     render() {
         return (<form />);
     }
 }
+
+
+
+Form.TextInput = TextInput;
 
 export default Form;
---
+++ src/components/form/TextInput.js
@@ -0,0 +1,13 @@
+// dependencies
+//
+import React from 'react';
+import classnames from 'classnames';
+
+
+class TextInput extends React.Component {
+    render() {
+        return (<div />);
+    }
+}
+
+export default TextInput;
