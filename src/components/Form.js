eb2bf47dd5313100e270235afe6e6867f9cb24f1
Dave Thomas <dave@monstergfx.com>
2016-09-16 09:47:03 -0700

    added basic component

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,10 @@
 import React from 'react';
 import classnames from 'classnames';
 
-class Form extends React.Component {}
+class Form extends React.Component {
+    render() {
+        return (<form />);
+    }
+}
 
 export default Form;
