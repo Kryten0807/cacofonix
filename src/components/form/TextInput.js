3498d3b3df5c5890aadc8856c821dd0f05768943
Dave Thomas <dave@monstergfx.com>
2016-09-17 15:07:32 -0700

    comment cleanup

--- src/components/form/TextInput.js
+++ src/components/form/TextInput.js
@@ -5,10 +5,11 @@ import classnames from 'classnames';
 import uniqueId from 'lodash/uniqueId';
 import isRegExp from 'lodash/isRegExp';
 
+// @TODO pattern = function which returns true if value passes
 // @TODO add different validation error messages for failing different rules
 // @TODO horizontal form - label & input element widths
 // @TODO permitted characters regex
-// @TODO pattern = function which returns true if value passes
+// @TODO parse & format functions for onFocus & onBlur handling of values
 
 /**
  * The TextInput component
