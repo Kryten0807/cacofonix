7f169435f863b1d77beefb7196384efb929eaf62
Dave Thomas <dave@monstergfx.com>
2016-09-20 11:09:56 -0700

    added options rendering

--- src/components/form/Dropdown.js
+++ src/components/form/Dropdown.js
@@ -3,6 +3,14 @@
 import React from 'react';
 import uniqueId from 'lodash/uniqueId';
 
-const Dropdown = () => (<select className="form-control" />);
+const Dropdown = ({ options }) => {
+    return (
+        <select className="form-control">
+            {options.map((opt) =>
+                <option key={uniqueId('form-dropdown-option-')} value={opt.value}>{opt.name}</option>
+            )}
+        </select>
+    );
+};
 
 export default Dropdown;
