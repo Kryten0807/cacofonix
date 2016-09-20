c4d1936011223d91706b8e5f6fb872bb26921bea
Dave Thomas <dave@monstergfx.com>
2016-09-20 12:08:45 -0700

    updated render method to handle invalid options

--- src/components/form/Dropdown.js
+++ src/components/form/Dropdown.js
@@ -19,7 +19,7 @@ class Dropdown extends React.Component {
         const label = this.props.label ? <label htmlFor={this.id}>{this.props.label}</label> : null;
 
         const select = (
-            <select id={this.id} className="form-control" value={this.props.value || this.props.options[0].value}>
+            <select id={this.id} className="form-control" value={isValidOption ? this.props.value : this.props.options[0].value}>
                 {this.props.options.map((opt) =>
                     <option key={uniqueId('form-dropdown-option-')} value={opt.value}>{opt.name}</option>
                 )}
