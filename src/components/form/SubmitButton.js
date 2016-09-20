e79d4bde03c05b3b24b6e6094da3687322b8cfe9
Dave Thomas <dave@monstergfx.com>
2016-09-20 07:49:53 -0700

    added proptypes

--- src/components/form/SubmitButton.js
+++ src/components/form/SubmitButton.js
@@ -6,4 +6,8 @@ const SubmitButton = (props) => (
     <button className="btn btn-default">{props.label || 'Submit'}</button>
 );
 
+SubmitButton.propTypes = {
+    label: React.PropTypes.string,
+};
+
 export default SubmitButton;
