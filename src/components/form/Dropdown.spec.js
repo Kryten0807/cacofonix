766fe715f8bc2ba4363297e5664f424c34d78622
Dave Thomas <dave@monstergfx.com>
2016-09-20 10:54:53 -0700

    added basic code for Dropdown spec

---
+++ src/components/form/Dropdown.spec.js
@@ -0,0 +1,22 @@
+// the eslint "no-unused-expressions" rule is triggered by the `expect`
+// assertions in the tests. So, in order to avoid problems with eslint, I'm
+// disabling that rule in this file
+//
+/* eslint-disable no-unused-expressions */
+
+import React from 'react';
+import { shallow, render, mount } from 'enzyme';
+import chai from 'chai';
+import sinon from 'sinon';
+
+import Form from '../Form';
+
+const expect = chai.expect;
+
+/* eslint-disable no-unused-vars */
+const debug = (component) => {
+    console.info('------------------------------------------------------------');
+    console.info(component.debug());
+    console.info('------------------------------------------------------------');
+};
+/* eslint-enable no-unused-vars */
