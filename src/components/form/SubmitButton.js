e7cc687064288e6549aa94e1885e466c85ca466d
Dave Thomas <dave@monstergfx.com>
2016-09-20 07:55:35 -0700

    added disabled prop to button

--- src/components/form/SubmitButton.js
+++ src/components/form/SubmitButton.js
@@ -3,7 +3,7 @@
 import React from 'react';
 
 const SubmitButton = (props) => (
-    <button className="btn btn-default">{props.label || 'Submit'}</button>
+    <button className="btn btn-default" disabled={false}>{props.label || 'Submit'}</button>
 );
 
 SubmitButton.propTypes = {
