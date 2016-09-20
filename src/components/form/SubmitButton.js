ba502316deb192f4734db74e8db1fe5d89f277b1
Dave Thomas <dave@monstergfx.com>
2016-09-20 07:48:49 -0700

    added default label

--- src/components/form/SubmitButton.js
+++ src/components/form/SubmitButton.js
@@ -2,6 +2,6 @@
 //
 import React from 'react';
 
-const SubmitButton = (props) => (<button className="btn btn-default">{props.label}</button>);
+const SubmitButton = (props) => (<button className="btn btn-default">{props.label || 'Submit'}</button>);
 
 export default SubmitButton;
