dfa484a7cc040db186daa7f1e56028a4f3699eec
Dave Thomas <dave@monstergfx.com>
2016-09-20 10:58:57 -0700

    implemented Dropdown component as part of Form package

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,6 +6,7 @@ import uniqueId from 'lodash/uniqueId';
 import classnames from 'classnames';
 
 import Alert from './Alert';
+import Dropdown from './form/Dropdown';
 import SubmitButton from './form/SubmitButton';
 import TextInput from './form/TextInput';
 
@@ -190,6 +191,7 @@ Form.childContextTypes = {
 // add the "sub-components" so that they can be imported as part of the same
 // package
 //
+Form.Dropdown = Dropdown;
 Form.SubmitButton = SubmitButton;
 Form.TextInput = TextInput;
 
