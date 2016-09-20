6a925e6cb857f59fbcdd391b6df6dbb988ef8c18
Dave Thomas <dave@monstergfx.com>
2016-09-20 08:42:28 -0700

    code cleanup

--- src/components/form/SubmitButton.js
+++ src/components/form/SubmitButton.js
@@ -2,8 +2,8 @@
 //
 import React from 'react';
 
-const SubmitButton = ({ label }, context) => (
-    <button className="btn btn-default" disabled={!context.isValid}>{label || 'Submit'}</button>
+const SubmitButton = ({ label }, { isValid }) => (
+    <button className="btn btn-default" disabled={!isValid}>{label || 'Submit'}</button>
 );
 
 SubmitButton.propTypes = {
