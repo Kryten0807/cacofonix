e12d3caedec9358914bf225689fc4c294572158b
Dave Thomas <dave@monstergfx.com>
2016-09-21 09:01:51 -0700

    added readOnly to proptypes

--- src/components/form/TextInput.js
+++ src/components/form/TextInput.js
@@ -209,6 +209,7 @@ class TextInput extends React.Component {
 //
 TextInput.propTypes = {
     required:               React.PropTypes.bool,
+    readOnly:               React.PropTypes.bool,
     id:                     React.PropTypes.string,
     value:                  React.PropTypes.string,
     label:                  React.PropTypes.string,
