b8f9fd6c4eeccaa10e5c0a99d2dfc832b5d6e6d1
Dave Thomas <dave@monstergfx.com>
2016-09-21 08:59:25 -0700

    updated notes

--- src/components/form/TextInput.spec.js
+++ src/components/form/TextInput.spec.js
@@ -25,6 +25,7 @@ a Form component with a TextInput element
     should not include a label if label is omitted
     should include a label if the label is set
     should include a placeholder if the placeholder is set
+    should include the readonly flag when readonly is set
 */
 describe('a Form component with a TextInput element', () => {
 
