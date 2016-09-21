e60ad986012e10bdfa02827505a2a859100d6f7c
Dave Thomas <dave@monstergfx.com>
2016-09-21 12:09:11 -0700

    added update helper

--- src/components/form/TextInput.js
+++ src/components/form/TextInput.js
@@ -1,6 +1,7 @@
 // dependencies
 //
 import React from 'react';
+import update from 'react-addons-update';
 import classnames from 'classnames';
 import uniqueId from 'lodash/uniqueId';
 import isRegExp from 'lodash/isRegExp';
