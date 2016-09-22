2f9ee063ed40e9a4dd075cb546907d0f9261415d
Dave Thomas <dave@monstergfx.com>
2016-09-22 11:23:14 -0700

    added options prop

--- src/components/form/CheckboxGroup.js
+++ src/components/form/CheckboxGroup.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import uniqueId from 'lodash/uniqueId';
 
-const CheckboxGroup = ({ label }) => (
+const CheckboxGroup = ({ label, options }) => (
     <div className="form-group">
         {label
             ? <label>{label}</label>
