3d123f36d94c0874c073fa20f4c6359310452ba0
Dave Thomas <dave@monstergfx.com>
2016-09-22 08:42:19 -0700

    converted component from stateless function to class

--- src/components/form/RadioButtonGroup.js
+++ src/components/form/RadioButtonGroup.js
@@ -4,19 +4,32 @@ import React from 'react';
 import uniqueId from 'lodash/uniqueId';
 
 
-const RadioButtonGroup = ({ label, options }) => (
-    <div>
-        {label ? (<label className="radiobuttongroup">{label}</label>) : null}
-        {options.map((opt) => (
-            <div key={uniqueId('form-radiobuttongroup-option-')} className="radio">
-                <label>
-                    <input type="radio" value={opt.value} />
-                    <span>{opt.name}</span>
-                </label>
-            </div>
-        ))}
     </div>
-);
 
+class RadioButtonGroup extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.id = uniqueId('form-radiobuttongroup-');
+    }
+
+    render() {
+        const { label, options } = this.props;
+
+        return (
+            <div>
+                {label ? (<label htmlFor={this.id} className="radiobuttongroup">{label}</label>) : null}
+                {options.map((opt) => (
+                    <div key={uniqueId('form-radiobuttongroup-option-')} className="radio">
+                        <label>
+                            <input type="radio" name={this.id} value={opt.value} />
+                            <span>{opt.name}</span>
+                        </label>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
 
 export default RadioButtonGroup;
