5f2c3e7251dbb0234df88f3e102172257769f2aa
Dave Thomas <dave@monstergfx.com>
2016-09-22 08:18:29 -0700

    updated notes

--- src/components/form/RadioButtonGroup.spec.js
+++ src/components/form/RadioButtonGroup.spec.js
@@ -24,6 +24,8 @@ const debug = (component) => {
 /* *****************************************************************************
 a Form component containing a RadioButtonGroup
     should include a <Form.RadioButtonGroup> as a child
+    should include a label if the label prop is set
+    should include a not label if the label prop is not set
 */
 describe('a Form component containing a RadioButtonGroup', () => {
 
