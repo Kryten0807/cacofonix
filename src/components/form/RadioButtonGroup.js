c65be076d48584cc1334bf67f04e72821ecd8cfd
Dave Thomas <dave@monstergfx.com>
2016-09-22 08:32:51 -0700

    added group label class

--- src/components/form/RadioButtonGroup.js
+++ src/components/form/RadioButtonGroup.js
@@ -6,8 +6,8 @@ import uniqueId from 'lodash/uniqueId';
 
 const RadioButtonGroup = ({ label, options }) => (
     <div>
-        {label ? (<label>{label}</label>) : null}
         {options.map((opt) => <div key={uniqueId('form-radiobuttongroup-option-')} className="radio"></div>)}
+        {label ? (<label className="radiobuttongroup">{label}</label>) : null}
     </div>
 );
 
