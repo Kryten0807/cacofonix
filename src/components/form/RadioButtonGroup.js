b00a09cda5dafecf2fc9e4e55812d0e4535c8aa6
Dave Thomas <dave@monstergfx.com>
2016-09-22 09:30:44 -0700

    comment cleanup

--- src/components/form/RadioButtonGroup.js
+++ src/components/form/RadioButtonGroup.js
@@ -3,27 +3,54 @@
 import React from 'react';
 import uniqueId from 'lodash/uniqueId';
 
+/**
+ * The RadioButtonGroup component
+ */
 class RadioButtonGroup extends React.Component {
+    /**
+     * Construct the component instance
+     * @param  {Object} props The component props
+     */
     constructor(props) {
         super(props);
 
+        // generate a unique ID for this element (used to tie the label to the
+        // input elements)
+        //
         this.id = uniqueId('form-radiobuttongroup-');
 
+        // build a list of permitted values from the options
+        //
         this.permittedValues = this.props.options.map((opt) => `${opt.value}`);
 
+        // initialize the component state
+        //
         this.state = {
             value: this.isPermittedValue(this.props.value)
                 ? `${this.props.value}`
                 : `${this.props.options[0].value}`,
         };
 
+        // bind `this` to the onChange event handler
+        //
         this.onChange = this.onChange.bind(this);
     }
 
+    /**
+     * Handle a change to the input elements
+     * @param  {Object} event The event object
+     */
     onChange(event) {
+        // get the current value & make sure it's a string
+        //
         const value = `${event.target.value}`;
 
+        // has the value changed? if so, then update the state & call the
+        // onChange handler
+        //
         if (value !== this.state.value) {
+            // update the component state
+            //
             this.setState({ value });
             if (this.props.onChange) {
                 this.props.onChange(event.target.value);
