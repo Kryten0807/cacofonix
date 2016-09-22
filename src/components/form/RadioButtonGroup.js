4629a7b91a0f9187893c94cffc9c10410783d7c4
Dave Thomas <dave@monstergfx.com>
2016-09-22 08:42:39 -0700

    code cleanup

--- src/components/form/RadioButtonGroup.js
+++ src/components/form/RadioButtonGroup.js
@@ -3,9 +3,6 @@
 import React from 'react';
 import uniqueId from 'lodash/uniqueId';
 
-
-    </div>
-
 class RadioButtonGroup extends React.Component {
     constructor(props) {
         super(props);
