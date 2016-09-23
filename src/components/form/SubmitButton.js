3fab9fd363c70a721b4da51daa0575762045db9d
Dave Thomas <dave@monstergfx.com>
2016-09-23 10:56:19 -0700

    implemented children

--- src/components/form/SubmitButton.js
+++ src/components/form/SubmitButton.js
@@ -24,7 +24,7 @@ const SubmitButton = ({ children, label, style, onClick }, { isValid }) => (
         disabled={!isValid}
         onClick={onClick}
     >
-        {label || 'Submit'}
+        {children || 'Submit'}
     </button>
 );
 
