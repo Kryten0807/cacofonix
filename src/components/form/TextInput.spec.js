d999d13644afbf81dfe0af80c784b9a8211a6ab1
Dave Thomas <dave@monstergfx.com>
2016-09-27 06:44:51 -0700

    code cleanup

--- src/components/form/TextInput.spec.js
+++ src/components/form/TextInput.spec.js
@@ -123,7 +123,8 @@ describe('a Form component with a TextInput element', () => {
         );
 
         expect(component.find('div.form-horizontal')).to.have.length(1, 'form-horizontal');
-        expect(component.find('div.form-horizontal div.form-group label').props().className).to.contain('col-xs-3', 'col-xs-3');
+        expect(component.find('div.form-horizontal div.form-group label').props().className)
+            .to.contain('col-xs-3', 'col-xs-3');
     });
 
     it('should have a div.col-xs-9 when form is horizontal and labelColumns=3', () => {
@@ -139,7 +140,8 @@ describe('a Form component with a TextInput element', () => {
         );
 
         expect(component.find('div.form-horizontal')).to.have.length(1, 'form-horizontal');
-        expect(component.find('div.form-textinput-input-columns').props().className).to.contain('col-xs-9', 'col-xs-9');
+        expect(component.find('div.form-textinput-input-columns').props().className)
+            .to.contain('col-xs-9', 'col-xs-9');
     });
 });
 
