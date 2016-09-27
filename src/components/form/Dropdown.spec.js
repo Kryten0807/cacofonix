ec285cce5b7381aa747911a0e8019ed4e0c3bf94
Dave Thomas <dave@monstergfx.com>
2016-09-27 08:24:08 -0700

    added spec for dropdown columns

--- src/components/form/Dropdown.spec.js
+++ src/components/form/Dropdown.spec.js
@@ -221,6 +221,31 @@ describe('a Form component containing a Dropdown', () => {
             .to.contain('col-xs-3', 'col-xs-3');
     });
 
+    it('should have a div.col-xs-9 when form is horizontal and labelColumns=3', () => {
+
+        const options = [
+            { value: '1', name: 'one' },
+            { value: '2', name: 'two' },
+            { value: '3', name: 'three' },
+        ];
+
+        const label = 'peter piper';
+        const columns = 3;
+
+        const value = 2;
+
+        const component = mount(
+            <Form horizontal labelColumns={columns}>
+                <Form.Dropdown label={label} options={options} value={value} />
+            </Form>
+        );
+
+        expect(component.find('div.form-horizontal')).to.have.length(1, 'form-horizontal');
+        expect(component.find('div.form-dropdown-columns').props().className)
+            .to.contain('col-xs-9', 'col-xs-9');
+    });
+
+
 });
 
 /* *****************************************************************************
