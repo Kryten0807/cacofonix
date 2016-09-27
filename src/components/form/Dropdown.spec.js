edbdc5d8542ae4d4b224fbe7bb9746357e94cc91
Dave Thomas <dave@monstergfx.com>
2016-09-27 08:08:26 -0700

    added spec for label columns

--- src/components/form/Dropdown.spec.js
+++ src/components/form/Dropdown.spec.js
@@ -196,6 +196,31 @@ describe('a Form component containing a Dropdown', () => {
 
         expect(component.find('select').props().value).to.equal(options[0].value);
     });
+
+    it('should have a label.col-xs-3 when form is horizontal and labelColumns=3', () => {
+
+        const options = [
+            { value: '1', name: 'one' },
+            { value: '2', name: 'two' },
+            { value: '3', name: 'three' },
+        ];
+
+        const label = 'peter piper';
+        const columns = 3;
+
+        const value = 2;
+
+        const component = mount(
+            <Form horizontal labelColumns={columns}>
+                <Form.Dropdown label={label} options={options} value={value} />
+            </Form>
+        );
+
+        expect(component.find('div.form-horizontal')).to.have.length(1, 'form-horizontal');
+        expect(component.find('label.control-label').props().className)
+            .to.contain('col-xs-3', 'col-xs-3');
+    });
+
 });
 
 /* *****************************************************************************
