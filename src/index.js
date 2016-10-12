0494e94a858b5b6a6de86c0c263838303ec68f3b
Dave Thomas <dave@monstergfx.com>
2016-10-12 10:39:58 -0700

    corrected references to deprecated components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 // export the individual components
 //
 export { default as Alert } from './components/Alert';
-export { default as DateInput } from './components/DateInput';
-export { default as Dropdown } from './components/Dropdown';
-export { default as Label } from './components/Label';
-export { default as NumericInput } from './components/NumericInput';
-export { default as Panel } from './components/Panel';
-export { default as TextInput } from './components/TextInput';
-
 export { default as Form } from './components/Form';
+export { default as Panel } from './components/Panel';
