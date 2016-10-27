6d2c14c13b0ea5b9fa307da62eeac14f9b3b0601
Dave Thomas <dave@monstergfx.com>
2016-10-27 07:39:17 -0700

    updated spec to use name prop instead of id

--- src/components/Form.spec.js
+++ src/components/Form.spec.js
@@ -130,19 +130,19 @@ describe('the Form component', () => {
             <Form>
                 <Form.TextInput
                     required
-                    id="1"
+                    name="textinput-1"
                     description={descriptionForValid}
                     value={initialValue}
                 />
                 <Form.TextInput
                     required
-                    id="2"
+                    name="textinput-2"
                     description={descriptionForInvalid}
                     value={initialValue}
                 />
                 <Form.TextInput
                     required
-                    id="3"
+                    name="textinput-3"
                     description={descriptionForValid}
                     value={initialValue}
                 />
@@ -152,11 +152,11 @@ describe('the Form component', () => {
 
         expect(component.find('Alert')).to.have.length(0);
 
-        component.find('input[id="2"]').simulate('change', {
+        component.find('input[name="textinput-2"]').simulate('change', {
             target: { value: newValue }
         });
 
-        component.find('input[id="2"]').simulate('blur');
+        component.find('input[name="textinput-2"]').simulate('blur');
 
         expect(component.find('Alert')).to.have.length(1);
         expect(component.find('Alert').text()).to.not.contain(descriptionForValid);
