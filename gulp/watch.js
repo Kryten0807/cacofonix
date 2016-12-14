4b9e3507711b44ee1efe67ad605bc22bb960f741
Dave Thomas <dave@monstergfx.com>
2016-12-14 10:20:53 -0800

    comment cleanup

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,8 +1,6 @@
-
+// import the paths
+//
 const paths = require('./paths.json');
 
-// -----------------------------------------------------------------------------
-// watch the files that need linting
-//
 module.exports = (gulp) =>
     gulp.watch(paths.lint, ['lint']);
