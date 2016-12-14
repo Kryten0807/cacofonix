7561b9a14005f12104f12952df964e462c112789
Dave Thomas <dave@monstergfx.com>
2016-12-14 09:43:52 -0800

    added build task

---
+++ gulp/tasks/build.js
@@ -0,0 +1,15 @@
+// gulp plugins/other support packages
+//
+const sequence = require('gulp-sequence');
+
+// -----------------------------------------------------------------------------
+// clean the client files
+//
+module.exports = (gulp, callback) =>
+    sequence(
+        'test',
+        'lint',
+        'clean',
+        'compile',
+        callback
+    );
