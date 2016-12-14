7e6ffc775d452aed78745f18f72dc44022fcdd9b
Dave Thomas <dave@monstergfx.com>
2016-12-14 09:58:28 -0800

    implemented paths.json

--- gulp/test.js
+++ gulp/test.js
@@ -1,11 +1,13 @@
 const mocha = require('gulp-mocha');
 
+const paths = require('./paths.json');
+
 module.exports = (gulp) => {
     require('babel-register'); // eslint-disable-line global-require
 
-    return gulp.src((process.env.TEST_SUITE || './src/**/*.spec.js'), { read: false })
+    return gulp.src((process.env.TEST_SUITE || paths.specs), { read: false })
         .pipe(mocha({
-            require:  ['./test/setup.js'],
+            require:  [paths.testconfig],
             // reporter: 'progress',
             // an alternate reporter is 'dot',
         }));
