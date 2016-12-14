4576b8cd1b02473f8ce73ac80bd410d0e0911edb
Dave Thomas <dave@monstergfx.com>
2016-12-14 09:30:13 -0800

    added new gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+// increase the number of listeners to prevent annoying warnings
+//
+require('events').EventEmitter.prototype._maxListeners = 30;
+
+// npm dependencies
+//
+const requireTasks = require('gulp-require-tasks');
+
+// initialize the task import
+//
+requireTasks({ path: `${__dirname}/gulp/tasks` });
