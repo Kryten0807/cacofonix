67576e67e12fcb9bf82156980c67e28682ed7068
Dave Thomas <dave@monstergfx.com>
2016-12-14 10:14:05 -0800

    comment cleanup

--- gulp/test.js
+++ gulp/test.js
@@ -1,8 +1,16 @@
+// gulp plugins/other support packages
+//
 const mocha = require('gulp-mocha');
 
+// import the paths
+//
 const paths = require('./paths.json');
 
 module.exports = (gulp) => {
+    // the test files are written in ES6. Import the babel-register package to
+    // transpile them as needed. I'm doing this here, rather than globally, so
+    // that the package is not loaded unless the test task is being run
+    //
     require('babel-register'); // eslint-disable-line global-require
 
     return gulp.src((process.env.TEST_SUITE || paths.specs), { read: false })
