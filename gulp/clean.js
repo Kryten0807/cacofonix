d2337498a85408f2f025f33f7440e8c1af86049d
Dave Thomas <dave@monstergfx.com>
2016-12-14 09:58:20 -0800

    implemented paths.json

--- gulp/clean.js
+++ gulp/clean.js
@@ -1,5 +1,7 @@
 const shell = require('gulp-shell');
 
+const paths = require('./paths.json');
+
 module.exports = (gulp) =>
-    gulp.src('./dist/**/*.js', { read: false })
+    gulp.src(`${paths.dist}/**/*.js`, { read: false })
         .pipe(shell('rm <%= file.path %>'));
