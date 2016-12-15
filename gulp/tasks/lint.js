b00409dc108fdb6c079a52b83b0bca235c8d819b
Dave Thomas <dave@monstergfx.com>
2016-12-15 08:41:21 -0800

    moved lintandfix helper to separate file

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -1,32 +1,13 @@
 // gulp plugins/other support packages
 //
 const eslint = require('gulp-eslint');
-const gulpIf = require('gulp-if');
 const yargs = require('yargs');
+
+const lintAndFix = require('../helpers/lintandfix');
 
 // import the paths
 //
 const paths = require('../paths.json');
-
-/**
- * A helper function to lint **and fix** a set of paths
- * @param  {Object} gulp The gulp instance
- * @param  {Array}  list The list of paths on which to operate
- * @return {Object}      The gulp stream
- */
-const lintAndFix = (gulp, list) => {
-    // declare a helper function to check if a file is fixed
-    //
-    const isFixed = (file) => file.eslint != null && file.eslint.fixed;
-
-    // lint the files in the path with the fix option & return the stream
-    //
-    return gulp.src(list, { base: './' })
-        .pipe(eslint({ fix: true }))
-        .pipe(gulpIf(isFixed, gulp.dest('./')))
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
-};
 
 module.exports = (gulp) => {
     // is the `--fix` flag set? if so, execute a "lint and fix"
