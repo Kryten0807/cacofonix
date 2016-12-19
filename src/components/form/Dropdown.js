588f4a008d8a4cc928544b14e03fff19b7280d05
Dave Thomas <dave@monstergfx.com>
2016-12-19 08:39:24 -0800

    updated code to accept array of strings for optgroups

--- src/components/form/Dropdown.js
+++ src/components/form/Dropdown.js
@@ -151,8 +151,8 @@ class Dropdown extends React.Component {
                 options.push(
                     <optgroup key={uniqueId('form-dropdown-optgroup-')} label={key}>
                         {this.props.options[key].map((opt) => (
-                            <option key={uniqueId('form-dropdown-option-')} value={opt.value}>
-                                {opt.name}
+                            <option key={uniqueId('form-dropdown-option-')} value={opt.value || opt}>
+                                {opt.name || opt}
                             </option>))
                         }
                     </optgroup>
