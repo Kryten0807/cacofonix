787c8662627ebb237a6083fb1be7ceda50e849af
Dave Thomas <dave@monstergfx.com>
2016-12-19 08:41:04 -0800

    code cleanup

--- src/components/form/Dropdown.js
+++ src/components/form/Dropdown.js
@@ -134,13 +134,11 @@ class Dropdown extends React.Component {
         if (isArray(this.props.options)) {
             // it's an array - simply map it to a list of <option> elements
             //
-            options = this.props.options.map((opt) => {
-                return (
-                    <option key={uniqueId('form-dropdown-option-')} value={opt.value || opt}>
-                        {opt.name || opt}
-                    </option>
-                );
-            });
+            options = this.props.options.map((opt) => (
+                <option key={uniqueId('form-dropdown-option-')} value={opt.value || opt}>
+                    {opt.name || opt}
+                </option>
+            ));
         } else {
             // it's an object - build a list of options, separated into
             // optgroups by key
